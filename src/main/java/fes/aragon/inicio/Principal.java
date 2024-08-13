2887628ef250d88a021fdeb30819e461b23d81ab
marvd <marvinzcalderon@gmail.com>
2024-08-12 21:17:04 -0600

    clase agregar terminada

--- src/main/java/fes/aragon/inicio/Principal.java
+++ src/main/java/fes/aragon/inicio/Principal.java
@@ -3,6 +3,6 @@ import fes.aragon.modelo.*;
 
 public class Principal {
     public static void main(String[] args) {
-
+        System.out.println("hola");
     }
 }
--- src/main/java/fes/aragon/modelo/agregar.java
+++ src/main/java/fes/aragon/modelo/agregar.java
@@ -1,4 +1,20 @@
 package fes.aragon.modelo;
 
 public class agregar {
+    public int indice;
+    public void insertar(int valor, int[] a) {
+if(!validarLongitud(a)){
+    a[indice]=valor;
+    indice++;
+}
+    }
+private boolean validarLongitud(int[]a){
+boolean limite=false;
+if(indice==a.length){
+    limite=true;
+}
+    return limite;
+}
+
+
 }
